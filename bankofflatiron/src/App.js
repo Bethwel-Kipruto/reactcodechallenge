20a3cb18152920cfb4d8b064a673db3337487842
BETHWEL <bethwelk85@gmail.com>
2023-04-26 08:11:42 +0300

    done

--- bankofflatiron/src/App.js
+++ bankofflatiron/src/App.js
@@ -7,9 +7,20 @@ import TransactionTable from './table';
 function App() {
 
 return(
-  <div>
- <TransactionForm />
+  <div style={{backgroundColor:'grey',
+               textAlign: "center"
+              
+  
+  }}>
+<h1  style={{
+    color:"white"
+}}>BANK OF FLATIRON</h1>
+<img  src="https://shorturl.at/uJMO5"  style={{
+    borderRadius: '15px'
+}}/>
 <TransactionTable />
+<TransactionForm />
+
 </div>
 
 
